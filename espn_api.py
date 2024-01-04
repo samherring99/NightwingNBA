0ffa32a0d093d007d647313a29934853dcb166ae
Sam Herring <samherring99@gmail.com>
2024-01-03 18:14:06 -0700

    Separating ESPN API util file into components

--- espn_api.py
+++ espn_api.py
@@ -13,12 +13,28 @@ def get_request(url):
     data = response.json()
     return data
 
+# This method finds the location of the requested statistic name in a given 
+# dictionary of NBA statistics for a given player. 
+# It returns the value of the requested statistic
+def get_player_average(stats, stat_name):
+    avg_value = 0
+
+    # Find the location of the statistic value given the name of the stat
+    avg_value_obj = next((item for item in stats if item["name"] == stat_name), None)
+
+    # Return the display value if it exists
+    if avg_value_obj is not None:
+        avg_value = avg_value_obj['displayValue']
+
+    return avg_value
+
+
 # This is the top level method that handles all operations to get ESPN statsitics for NBA players
 def create_nba_database():
 
     # Get the list of NBA teams from ESPN 
-    url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams"
-    teams_data = get_request(url)
+    nba_url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams"
+    teams_data = get_request(nba_url)
 
     # Iterate over the teams in the list
     for team in teams_data["sports"][0]["leagues"][0]["teams"]:
@@ -26,8 +42,8 @@ def create_nba_database():
         print(team["team"]["displayName"])
 
         # Get the roster of the team we are currently on when iterating through teams
-        url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/{id}/roster".format(id=team["team"]["id"])
-        players_roster = get_request(url)
+        team_url = "https://site.api.espn.com/apis/site/v2/sports/basketball/nba/teams/{id}/roster".format(id=team["team"]["id"])
+        players_roster = get_request(team_url)
 
         # Iterate over the players in the roster
         print("Getting player stats")
@@ -36,9 +52,9 @@ def create_nba_database():
             print(athlete["fullName"])
 
             # Get the stats for the player we are on when iterating over the players in the team we are currently looking at
-            url3 = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/athletes/{id}/statistics".format(id=athlete["id"])
-            response3 = requests.get(url3)
-            player = response3.json()
+            player_url = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/athletes/{id}/statistics".format(id=athlete["id"])
+            response = requests.get(player_url)
+            player = response.json()
 
             # Storage variables
             avg_points = 0
@@ -46,28 +62,16 @@ def create_nba_database():
             avg_assists = 0
 
             # If we get data, iterate over the stats of the player
-            if response3.status_code != 404:
+            if response.status_code != 404:
                 for stattype in player["splits"]["categories"]: #General, Defensive, Offensive
                     for stat in stattype["stats"]:
                         if stat["name"] in ["avgPoints", "avgAssists", "avgRebounds"]:
                             print(stat["displayName"] + " " + str(stat["value"]))
 
-                    # Store average points, assists, and rebounds in an object
-
-                    avg_points_obj = next((item for item in stattype["stats"] if item["name"] == "avgPoints"), None)
-                    avg_rebounds_obj = next((item for item in stattype["stats"] if item["name"] == "avgRebounds"), None)
-                    avg_assists_obj = next((item for item in stattype["stats"] if item["name"] == "avgAssists"), None)
-
-                    # Assign the display value of the object to the storage variables created above
-
-                    if avg_assists_obj is not None:
-                        avg_points = avg_points_obj["displayValue"]
-                    
-                    if avg_rebounds_obj is not None:
-                        avg_rebounds = avg_rebounds_obj["displayValue"]
-
-                    if avg_assists_obj is not None:
-                        avg_assists = avg_assists_obj["displayValue"]
+                    # Update the storage variables
+                    avg_points = get_player_average(stattype["stats"], "avgPoints")
+                    avg_rebounds = get_player_average(stattype["stats"], "avgRebounds")
+                    avg_assists = get_player_average(stattype["stats"], "avgAssists")
 
                 # Create an entry for the player
                 player_data = (athlete["fullName"], str(team["team"]["displayName"]), "Team", str(avg_points), str(avg_rebounds), str(avg_assists), '0', '0', '0', '0', '0', '0', '0', '0', '0', datetime.today().strftime('%Y-%m-%d'))
