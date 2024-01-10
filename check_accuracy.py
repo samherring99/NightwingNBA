e525d89e07b350c80a6f2303f0f9827901d0e11f
Sam Herring <samherring99@gmail.com>
2024-01-09 18:55:56 -0700

    Small improvements to validation file

--- check_accuracy.py
+++ check_accuracy.py
@@ -1,5 +1,17 @@
 import requests
 
+game_id_endpoint = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/teams/"
+game_page_response = requests.get(game_id_endpoint)
+game_page = game_page_response.json()
+# url, params={"page": page_index}
+print(game_page['pageCount'])
+print(game_page['items'][0]['$ref'])
+print("ID: " + str(game_page['items'][0]['$ref']).split("?")[0][-1])
+
+# TODO page counts iteration
+
+# Using each ID, we want to get the games for each team ID using the below URL
+
 # First we want to read the picks in to a dict in this file
 url = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/20/events?lang=en&region=us"
 
@@ -9,11 +21,16 @@ def get_game_date(game_id):
     data = response.json()
     print(data['date'])
 
-test = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/events/401585087/competitions/401585087/competitors/4/roster/4395651/statistics/0?lang=en&region=us"
+#test = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/events/401585087/competitions/401585087/competitors/4/roster/4395651/statistics/0?lang=en&region=us"
 
 page_index = 1
 response = requests.get(url, params={"page": page_index}) # url, params={"page": page_index}
 data = response.json()
+
+# TODO page counts iteration with the above, return more data than just date
+
+# Iterate over the games for each team here, and get the ID number of each game
+# Use the method call to return game data
 
 page_count = data['pageCount']
 for item in data['items'][:1]:
@@ -22,3 +39,5 @@ for item in data['items'][:1]:
     get_game_date(id_num)
 
     #Sleep for rate limiting
+
+# Then validation pieces
