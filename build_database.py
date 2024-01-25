2d947fdda2df94b06b6727cd3b94730cb45ce3b9
Sam Herring <samherring99@gmail.com>
2024-01-25 14:01:00 -0700

    Small updates to build database file

--- build_database.py
+++ build_database.py
@@ -38,26 +38,23 @@ def process_game(game_id, data, cursor, conn):
         time.sleep(0.1)
         return id_num
 
-# TODO This could use some methodizing
+def process_team_data(data, game_id, cursor, conn):
+    for game in data['items']:
+        process_game(game_id, data, cursor, conn)
 
 def iterate_teams(team_list, cursor, connection):
-    seen_games = []
 
     for id_num in team_list:
         team_games_endpoint = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us".format(team_id=id_num)
         team_games_response = requests.get(team_games_endpoint, params={"page": 1})
         team_game_data = team_games_response.json()
         page_count = team_game_data['pageCount']
-        for game in team_game_data['items']:
-            game_id = process_game(id_num, team_game_data, cursor, connection)
-            seen_games.append(game_id)
+        process_team_data(team_game_data, id_num, cursor, connection)
     
         for i in range(page_count-1):
             team_games_response = requests.get(team_games_endpoint, params={"page": int(i+2)})
             team_game_data = team_games_response.json()
-            for i in team_game_data['items']:
-                game_id = process_game(id_num, team_game_data, cursor, connection)
-                seen_games.append(game_id)
+            process_team_data(team_game_data, id_num, cursor, connection)
                     
 create_database(cursor, conn)
 iterate_teams(get_all_teams(), cursor, conn)
