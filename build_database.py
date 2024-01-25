0c1d95407574772ab1e6fd2a13a89b907e827761
Sam Herring <samherring99@gmail.com>
2024-01-25 14:24:43 -0700

    Finished splitting out build database code

--- build_database.py
+++ build_database.py
@@ -2,6 +2,7 @@ from utils.misc_utils import get_all_teams
 from utils.player_stat_utils import get_game_stats_by_player
 from utils.sql_utils import write_players_data_to_db, write_team_data_to_db, create_database
 from utils.team_stat_utils import get_game_stats_for_teams
+from utils.processing_utils import write_player_and_team_data, process_game, process_team_data
 
 import requests
 import sqlite3
@@ -12,49 +13,21 @@ conn = sqlite3.connect('nba_datastore.db',
                              sqlite3.PARSE_COLNAMES)
 cursor = conn.cursor()
 
-# Player or team
-
-def write_player_and_team_data(player_data, team_data, game_id, cursor):
-    write_players_data_to_db(player_data, cursor)
-    print("Success adding player data!")
-
-    game_team_stats = get_game_stats_for_teams(game_id, player_data['date']) 
-
-    if game_team_stats:
-        write_team_data_to_db(team_data, cursor)
-        print("Success adding team data!")
-
-# Top level method for database building
-
-def process_game(game_id, data, cursor, conn):
-    id_num = str(game['$ref']).split("?")[0].split("/")[-1]
-    if id_num not in seen_games:
-        game_players_stats = get_game_stats_by_player(id_num)
-
-        if game_players_stats:
-            write_player_and_team_data(game_players_stats, data, id_num, cursor)
-
-        conn.commit()
-        time.sleep(0.1)
-        return id_num
-
-def process_team_data(data, game_id, cursor, conn):
-    for game in data['items']:
-        process_game(game_id, data, cursor, conn)
-
 def iterate_teams(team_list, cursor, connection):
+
+    seen_games = []
 
     for id_num in team_list:
         team_games_endpoint = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us".format(team_id=id_num)
         team_games_response = requests.get(team_games_endpoint, params={"page": 1})
         team_game_data = team_games_response.json()
         page_count = team_game_data['pageCount']
-        process_team_data(team_game_data, id_num, cursor, connection)
+        process_team_data(team_game_data, seen_games, cursor, connection)
     
         for i in range(page_count-1):
             team_games_response = requests.get(team_games_endpoint, params={"page": int(i+2)})
             team_game_data = team_games_response.json()
-            process_team_data(team_game_data, id_num, cursor, connection)
+            process_team_data(team_game_data, seen_games, cursor, connection)
                     
 create_database(cursor, conn)
 iterate_teams(get_all_teams(), cursor, conn)
