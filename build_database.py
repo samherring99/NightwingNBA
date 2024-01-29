50bb7756aa34a86cf43413374ec30d948003123c
Sam Herring <samherring99@gmail.com>
2024-01-29 11:07:20 -0700

    Minor changes to build database code

--- build_database.py
+++ build_database.py
@@ -13,20 +13,23 @@ conn = sqlite3.connect('nba_datastore.db',
                              sqlite3.PARSE_COLNAMES)
 cursor = conn.cursor()
 
+ENDPOINT="http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us"
+
+def get_team_data(page, team_id):
+    team_games_response = requests.get(ENDPOINT.format(team_id=team_id), params={"page": page})
+    team_game_data = team_games_response.json() 
+
 def iterate_teams(team_list, cursor, connection):
 
     seen_games = []
 
     for id_num in team_list:
-        team_games_endpoint = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us".format(team_id=id_num)
-        team_games_response = requests.get(team_games_endpoint, params={"page": 1})
-        team_game_data = team_games_response.json()
+        team_game_data = get_team_data(1, id_num)
         page_count = team_game_data['pageCount']
         process_team_data(team_game_data, seen_games, cursor, connection)
     
         for i in range(page_count-1):
-            team_games_response = requests.get(team_games_endpoint, params={"page": int(i+2)})
-            team_game_data = team_games_response.json()
+            team_game_data = get_team_data(int(i+2), id_num)
             process_team_data(team_game_data, seen_games, cursor, connection)
                     
 create_database(cursor, conn)
