ff4b087e2033f9d5edf863dc895c4b63bc4f3977
Sam Herring <samherring99@gmail.com>
2024-01-31 11:45:33 -0700

    Organizaiton of database updating methods

--- update_database.py
+++ update_database.py
@@ -14,19 +14,25 @@ conn = sqlite3.connect('nba_datastore.db',
                              sqlite3.PARSE_COLNAMES)
 cursor = conn.cursor()
 
-def iterate_teams(team_list, cursor, conn):
+ENDPOINT="http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us"
 
+# This method takes in a page number and team ID and returns the 
+# list of games for that page of the team's games this season
+def process_game_page(page, team_id):
+    team_games_response = requests.get(ENDPOINT.format(team_id=team_id), params={"page": page})
+    team_game_data = team_games_response.json()
+    add_games(team_game_data, cursor, conn)
+    return team_game_data
+
+# For each team ID, process through each page of their game IDs this season
+# This method calls the above process_game_page, which in turn calls add_games to add games to the database
+def iterate_teams(team_list, cursor, conn):
     for id_num in team_list:
-        team_games_endpoint = "http://sports.core.api.espn.com/v2/sports/basketball/leagues/nba/seasons/2024/teams/{team_id}/events?lang=en&region=us".format(team_id=id_num)
-        team_games_response = requests.get(team_games_endpoint, params={"page": 1})
-        team_game_data = team_games_response.json()
+        team_game_data = process_game_page(1, id_num)
         page_count = team_game_data['pageCount']
-        add_games(team_game_data, cursor, conn)
     
         for i in range(page_count-1):
-            team_games_response = requests.get(team_games_endpoint, params={"page": int(i+2)})
-            team_game_data = team_games_response.json()
-            add_games(team_game_data, cursor, conn)
+            team_game_data = process_game_page(int(i+2), id_num)
                     
 create_database(cursor, conn)
 iterate_teams(get_all_teams(), cursor, conn)
